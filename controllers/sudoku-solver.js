b50e19a1dbbbc2c1542b0e5b9ab5a1f45686f8bf
MartinElsaesser <marels@aon.at>
2022-01-22 17:18:58 +0100

    Changed comparison operator in check to allow json response to be of type String and Number

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -80,7 +80,7 @@ class SudokuSolver {
 		let rows = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9 }
 		let rowAsNumber = rows[row];
 		let cellI = (rowAsNumber - 1) * 9 + columnAsNumber - 1;
-		if (puzzleString[cellI] === value) {
+		if (puzzleString[cellI] == value) {
 			var temp = puzzleString.split('');
 			temp[cellI] = ".";
 			puzzleString = temp.join('');
